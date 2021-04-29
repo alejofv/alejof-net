cc072a2d4f36f861b1e7428765804a6b60d970b7
Alejandro Figueroa <alexphi@gmail.com>
2021-04-28 22:18:40 -0500

    Gridsome scaffolding and tweaks

---
+++ src/layouts/Default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container max-w-screen-md mx-auto px-5">
+    <header class="flex justify-between items-center mt-8 mb-12">
+      <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong>
+      <nav class="space-x-6">
+        <g-link class="nav__link" to="/">Home</g-link>
+      </nav>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.html',
+      './src/**/*.pug',
+      './src/**/*.md'
+    ]
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
