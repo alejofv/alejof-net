80ccf8c4d82790830ee85a141887d19563e4696b
Alejandro Figueroa <alexphi@gmail.com>
2021-05-24 17:46:45 -0500

    Another one

--- src/components/links/NavLink.vue
+++ src/components/links/NavLink.vue
@@ -1,9 +1,11 @@
 <template>
-  <g-link
-    class="text-sm uppercase hover:text-gray-500 hover:border-gray-500"
-    :to="to"
-    ><slot
-  /></g-link>
+  <span>
+    <g-link
+      class="text-sm uppercase hover:text-gray-500 hover:border-gray-500"
+      :to="to"
+      ><slot />
+    </g-link>
+  </span>
 </template>
 
 <script>
--- src/components/links/RouteLink.vue
+++ src/components/links/RouteLink.vue
@@ -1,17 +1,19 @@
 <template>
-  <g-link
-    class="hover:text-gray-500"
-    :class="(border ? 'border-b border-gray-800 dark:border-gray-100' : '')"
-    :to="to"
-    ><slot
-  /></g-link>
+  <span>
+    <g-link
+      class="hover:text-gray-500"
+      :class="border ? 'border-b border-gray-800 dark:border-gray-100' : ''"
+      :to="to"
+      ><slot />
+    </g-link>
+  </span>
 </template>
 
 <script>
 export default {
   props: {
     to: String,
-    border: Boolean
+    border: Boolean,
   },
 };
 </script>
